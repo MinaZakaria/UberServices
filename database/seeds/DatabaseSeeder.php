98bba650a5a4fb82f7fcfe001d158d56d17e3760
Mina Zakaria Zakher Said <mina.zakaria@enozom.com>
2017-10-17 12:06:12 +0200

    Merge origin/Users into user-frontend
    Conflicts:
    	js/app.js
    	js/controllers/company/priceComparison/PriceComparisonController.js
    	views/company/priceComparison/price_comparison.html

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,16 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(AccountStatusTableSeeder::class);
-         $this->call(AgeTableSeeder::class);
-         $this->call(AreaTableSeeder::class);
-         $this->call(CityTableSeeder::class);
-         $this->call(CompaniesTableSeeder::class);
-         $this->call(GenderTableSeeder::class);
-         $this->call(MaritalStatusesTableSeeder::class);
-         $this->call(MilitaryStatusesTableSeeder::class);
-         $this->call(PostTableSeeder::class);
-         $this->call(RoleTableSeeder::class);
-         $this->call(UserTableSeeder::class);
+//         $this->call(UserTableSeeder::class);
     }
 }
